4610f9b126bd8608eddf19c1adc71c7b98d7a337
Aman <aman.kumar@ayuvya.com>
2025-12-16 21:42:47 +0530

    first commit

---
+++ src/main/java/com/amandhanda/projects/Crafty/CraftyApplication.java
@@ -0,0 +1,13 @@
+package com.amandhanda.projects.Crafty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CraftyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CraftyApplication.class, args);
+	}
+
+}
